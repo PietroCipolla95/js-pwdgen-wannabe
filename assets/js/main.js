02823dac11e9edae9fd5afa38644de5ec05f2868
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-08-22 15:32:14 +0200

    finish
    i think

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 //Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito ed infine scrivi in pagina (non solo in console) il risultato
 
-const userName = prompt ('type your name')
+const userName = prompt ('type your name');
 
-const userSurname = prompt ('type your surname')
+const userSurname = prompt ('type your surname');
 
-const favColour = prompt ('type your favorite colour')
+const favColour = prompt ('type your favorite colour');
 
-console.log(userName + userSurname + favColour);
+console.log(userName + userSurname + favColour + '23');
 
+document.getElementById('infos').innerHTML =  (userName + userSurname + favColour + '23');
 
 
