b2cf82534dee8777b6616ecb5da7b55cff779113
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-08-22 15:24:15 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+//Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito ed infine scrivi in pagina (non solo in console) il risultato
+
+const userName = prompt ('type your name')
+
+const userSurname = prompt ('type your surname')
+
+const favColour = prompt ('type your favorite colour')
+
+console.log(userName + userSurname + favColour);
+
+
+
